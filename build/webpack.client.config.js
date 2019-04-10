5597bb6c8694fdc3241bdb692773c3af43933e95
张兆勇 <zhangzhaoyong@mtgr1020MacBook-Pro.local>
2019-04-10 15:24:34 +0800

    修复不能模块热替换

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -4,7 +4,9 @@ const baseConfig = require('./webpack.base.config')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 module.exports = merge(baseConfig, {
-    entry: './src/entry-client.js',
+    entry: {
+        app:'./src/entry-client.js'
+    },
     
     //Since webpack v4, the CommonsChunkPlugin was removed in favor of optimization.splitChunks
     optimization: {
